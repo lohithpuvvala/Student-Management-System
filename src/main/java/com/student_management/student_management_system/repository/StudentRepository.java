d1a62d263052fb80be4a8fbb7fe0c25511984a02
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-05 02:12:42 +0530

    Create Student Repository. Create Query Method findByEmail return Optional<Student>

---
+++ src/main/java/com/student_management/student_management_system/repository/StudentRepository.java
@@ -0,0 +1,21 @@
+package com.student_management.student_management_system.repository;
+
+import com.student_management.student_management_system.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+/**
+ * Repository interface for Student entity.
+ * Extends JpaRepository for basic CRUD operations and JpaSpecificationExecutor for querydsl support
+ */
+
+public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
+
+    //Custom query method to find student by email
+    //@param email unique email of the student
+    //@return Optional<Student> containing the student if found
+
+    Optional<Student> findByEmail(String name);
+}
