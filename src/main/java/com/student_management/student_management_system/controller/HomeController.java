f3e71fa18b9c12d0d554cfd1453ae970375c26a6
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-05 01:09:48 +0530

    Create Spring Project: Student Management System.

---
+++ src/main/java/com/student_management/student_management_system/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.student_management.student_management_system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Student Management System!";
+    }
+}
