316d32e4263eb7797fd1ae3f7152f2ae39117842
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-05 02:00:17 +0530

    Update Student Entity with Constraints

--- src/main/java/com/student_management/student_management_system/model/Student.java
+++ src/main/java/com/student_management/student_management_system/model/Student.java
@@ -1,6 +1,9 @@
 package com.student_management.student_management_system.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -10,6 +13,9 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.SortedSet;
 import java.util.TreeSet;
+
+import static jakarta.persistence.ConstraintMode.NO_CONSTRAINT;
+
 
 @Data
 @Entity
@@ -21,13 +27,17 @@ public class Student {
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 50)
+    @Column(name = "name", length = 50)
+    @NotNull(message = "Name is required")
     private String name;
 
-    @Column(name = "email", unique = true ,nullable = false)
+    @Column(name = "email", unique = true , updatable = true)
+    @NotNull(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    @Column(name = "date_of_birth", columnDefinition = "DATE")
+    @Column(name = "date_of_birth")
+    @Past(message = "Date of birth must be in the past")
     private LocalDate dateOfBirth;
 
     @CreationTimestamp
