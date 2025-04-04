e19ad535591273e14272f88530b09d58eb573a8d
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-05 01:45:07 +0530

    Update Student Entity. Create SortedSet courses (many-to-many) with Set Students in Course. Bidirectional relationship

--- src/main/java/com/student_management/student_management_system/model/Student.java
+++ src/main/java/com/student_management/student_management_system/model/Student.java
@@ -6,7 +6,10 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 @Data
 @Entity
@@ -34,4 +37,12 @@ public class Student {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_course",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private SortedSet<Course> courses = new TreeSet<>(Comparator.comparing(Course::getCode));
 }
