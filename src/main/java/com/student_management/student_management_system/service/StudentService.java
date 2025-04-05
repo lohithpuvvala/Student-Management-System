b1369085a98beed01c03c78b1bf14b1258125a7f
lohithpuvvala <lohithpuvvala@gmail.com>
2025-04-05 13:13:41 +0530

    Update StudentService. Create Methods findStudentByID(Long ID), saveStudent(Student student), updateStudent(Student student), deleteStudent(Long ID

--- src/main/java/com/student_management/student_management_system/service/StudentService.java
+++ src/main/java/com/student_management/student_management_system/service/StudentService.java
@@ -2,6 +2,7 @@ package com.student_management.student_management_system.service;
 
 import com.student_management.student_management_system.model.Student;
 import com.student_management.student_management_system.repository.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -34,4 +35,64 @@ public class StudentService {
     }
 
     //Method to get Single Student by ID
+    public Student getStudentById(Long id) {
+        log.info("Fetching student by id: {}", id);
+        try{
+            Student student = studentRepository
+                                .findById(id)
+                                .orElseThrow(() ->  new EntityNotFoundException("Student not found with id: "+ id));
+            return student;
+        }catch (Exception e){
+            log.error("Error fetching student with ID: {}"+id+e);
+            throw new RuntimeException("Failed to fetch student",e);
+        }
+    }
+
+    //Method to save a new student
+    public Student saveStudent(Student student) {
+        log.info("Saving new student: {}", student.getName());
+        try{
+            return studentRepository.save(student);
+        }catch (Exception e){
+            log.error("Error saving student: {}", student.getName(),e);
+            throw new RuntimeException("Failed to save student",e);
+        }
+    }
+
+    //Method to update an existing student.
+    public Student updateStudent(Student student) {
+        log.info("Updating student: {}", student.getName());
+        try{
+            Student existingStudent = studentRepository
+                    .findById(student.getId())
+                    .orElseThrow(() ->  new EntityNotFoundException("Student not found with id: "+ student.getId()));
+            return studentRepository.save(existingStudent);
+        }catch (Exception e){
+            log.error("Error updating student: {}", student.getName(),e);
+            throw new RuntimeException("Failed to update student",e);
+        }
+    }
+
+    public void deleteStudent(Long id) {
+        log.info("Deleting student: {}", id);
+        try{
+            Student student = studentRepository
+                    .findById(id)
+                    .orElseThrow(() ->  new EntityNotFoundException("Student not found with id: "+ id));
+            studentRepository.delete(student);
+        }catch (Exception e){
+            log.error("Error deleting student with ID: {}", id,e);
+            throw new RuntimeException("Failed to delete student",e);
+        }
+
+
+        //Custom exception class for resource not found scenarios
+        @SuppressWarnings("serial")
+         class ResourceNotFoundException extends EntityNotFoundException{
+            public ResourceNotFoundException(String message){
+                super(message);
+            }
+        }
+
+    }
 }
